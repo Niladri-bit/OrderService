b313ba711cd46c5406fa31e16c41e30c11f25886
NILADRI BIT <niladribit1@gmail.com>
2024-11-01 18:30:55 +0530

    fixing update stock call to book microservice

--- src/main/java/com/assignment/order/service/OrderService/service/BookCommunicationService.java
+++ src/main/java/com/assignment/order/service/OrderService/service/BookCommunicationService.java
@@ -1,6 +1,9 @@
 package com.assignment.order.service.OrderService.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
@@ -32,13 +35,19 @@ public class BookCommunicationService {
 
         try {
         	String url = BOOK_SERVICE_URL + "/" + id + "/stock";
-            StockUpdateRequestDTO stockUpdateRequest = new StockUpdateRequestDTO();
-            stockUpdateRequest.setStock(newStock);
-            restTemplate.put(url, stockUpdateRequest);
+        	 HttpHeaders headers = new HttpHeaders();
+             headers.set("X-INTERNAL", "ROLE_SERVICE_COMMUNICATOR");
+
+             StockUpdateRequestDTO stockUpdateRequest = new StockUpdateRequestDTO();
+             stockUpdateRequest.setStock(newStock);
+
+             HttpEntity<StockUpdateRequestDTO> requestEntity = new HttpEntity<>(stockUpdateRequest, headers);
+
+             restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Void.class);
         }
         catch ( HttpStatusCodeException e) {
             System.err.println(e.getMessage());
-            throw new BookNotFoundException("Book not found while ordering");
+            throw new BookNotFoundException("Creation of order failed maybe due to book not found or updating stock");
         }
     }
 }
